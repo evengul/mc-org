397f1eb0e9b4d3fe2e901b5ae6ba4baf026141cd
Even Gultvedt <even.gultvedt@gmail.com>
2023-08-30 21:21:37 +0200

    fix: Typed the dispatcher correctly.
    TODO: Implement an actual messaging system, this won't work inter-system.

--- mcorg.backends/permission-service/src/main/java/app/mcorg/permission/infrastructure/dispatch/PermissionEventDispatcher.java
+++ mcorg.backends/permission-service/src/main/java/app/mcorg/permission/infrastructure/dispatch/PermissionEventDispatcher.java
@@ -1,18 +1,18 @@
 package app.mcorg.permission.infrastructure.dispatch;
 
-import app.mcorg.common.event.DomainEvent;
 import app.mcorg.common.event.EventDispatcher;
+import app.mcorg.common.event.permission.PermissionEvent;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class PermissionEventDispatcher implements EventDispatcher {
+public class PermissionEventDispatcher implements EventDispatcher<PermissionEvent> {
     private final ApplicationEventPublisher publisher;
 
     @Override
-    public void dispatch(DomainEvent event) {
+    public void dispatch(PermissionEvent event) {
         publisher.publishEvent(event);
     }
 }
