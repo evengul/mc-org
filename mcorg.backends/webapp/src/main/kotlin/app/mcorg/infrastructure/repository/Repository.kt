23195e39607557afa199e120bb26028471686f6c
evegul <even.gultvedt@sparebank1.no>
2024-06-13 10:15:22 +0200

    DB Handling for production

--- mcorg.backends/webapp/src/main/kotlin/app/mcorg/infrastructure/repository/Repository.kt
+++ mcorg.backends/webapp/src/main/kotlin/app/mcorg/infrastructure/repository/Repository.kt
@@ -3,16 +3,36 @@ package app.mcorg.infrastructure.repository
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import java.sql.Connection
+import java.sql.DriverManager
 
-private val dataSource = HikariDataSource(HikariConfig().apply {
-    jdbcUrl = System.getenv("DB_URL")
-    username = System.getenv("DB_USER")
-    password = System.getenv("DB_PASSWORD")
-    driverClassName = "org.postgresql.Driver"
-})
+private var dataSource: HikariDataSource? = null
 
 open class Repository {
     fun getConnection(): Connection {
-        return dataSource.connection
+        val env = System.getenv("ENV")
+
+        if (env == "LOCAL") {
+            if (dataSource == null) {
+                dataSource = HikariDataSource(HikariConfig().apply {
+                    val config = Config.get()
+                    jdbcUrl = config.url
+                    username = config.password
+                    password = config.password
+                    driverClassName = "org.postgresql.Driver"
+                })
+            }
+            return dataSource!!.connection
+        }
+
+        val (url, username, password) = Config.get()
+        return DriverManager.getConnection(url, username, password)
+    }
+}
+
+data class Config(val url: String, val user: String, val password: String) {
+    companion object {
+        fun get(): Config {
+            return Config(System.getenv("DB_URL"), System.getenv("DB_USER"), System.getenv("DB_PASSWORD"))
+        }
     }
 }
