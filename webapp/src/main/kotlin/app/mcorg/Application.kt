9778d7fa50eba7d1db407adf7a4ebc887b4856f7
evengul <44507133+evengul@users.noreply.github.com>
2024-10-14 14:44:12 +0200

    feat: Migrate DB on startup. (#43)
    Co-authored-by: evegul <even.gultvedt@sparebank1.no>

--- webapp/src/main/kotlin/app/mcorg/Application.kt
+++ webapp/src/main/kotlin/app/mcorg/Application.kt
@@ -5,8 +5,20 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import app.mcorg.presentation.plugins.*
 import app.mcorg.presentation.configureAppRouter
+import org.flywaydb.core.Flyway
+import org.slf4j.LoggerFactory
 
 fun main() {
+    val logger = LoggerFactory.getLogger(Application::class.java)
+    logger.info("Starting DB migration")
+    Flyway.configure()
+        .dataSource(System.getenv("DB_URL"), System.getenv("DB_USER"), System.getenv("DB_PASSWORD"))
+        .locations("db/migration")
+        .load()
+        .migrate()
+    logger.info("DB Migration complete")
+
+    logger.info("Starting server")
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module, watchPaths = getWatchPaths())
             .start(wait = true)
 }
