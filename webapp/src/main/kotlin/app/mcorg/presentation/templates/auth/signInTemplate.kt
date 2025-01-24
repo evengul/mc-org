8d69e2d8b7dd54aec3f352ced52fd6c3d7421700
evegul <even.gultvedt@sparebank1.no>
2025-01-24 10:11:19 +0100

    Better sign in page

--- webapp/src/main/kotlin/app/mcorg/presentation/templates/auth/signInTemplate.kt
+++ webapp/src/main/kotlin/app/mcorg/presentation/templates/auth/signInTemplate.kt
@@ -6,24 +6,28 @@ import kotlinx.html.*
 fun signInTemplate(microsoftUrl: String): String = baseTemplate("MC-ORG | Sign In") {
     main {
         id = "sign-in"
-        h1 {
-            id = "sign-in-header"
-            + "Welcome to MCORG!"
-        }
-        p {
-            + "This app aims to solve all your project planning issues in Minecraft, along with much else."
-        }
-        p {
-            + "To start, sign in with the personal Microsoft account you have connected to Minecraft, and we will pull up your username to start your planning journey."
-        }
-        a {
-            id = "sign-in-link"
-            href = microsoftUrl
-            button {
-                span {
-                    classes = setOf("icon", "icon-microsoft")
+        div {
+            classes = setOf("sign-in-container")
+            h1 {
+                id = "sign-in-header"
+                + "Welcome to MCORG!"
+            }
+            p {
+                + "This app aims to solve all your project planning issues in Minecraft, along with much else."
+            }
+            p {
+                + "To start, sign in with the personal Microsoft account you have connected to Minecraft, and we will pull up your username to start your planning journey."
+            }
+            a {
+                id = "sign-in-link"
+                href = microsoftUrl
+                button {
+                    id = "microsoft-button"
+                    span {
+                        classes = setOf("icon", "icon-microsoft")
+                    }
+                    + "Sign in with Microsoft"
                 }
-                + "Sign in with Microsoft"
             }
         }
     }
