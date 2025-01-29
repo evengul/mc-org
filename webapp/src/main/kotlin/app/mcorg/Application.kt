e4c8edc1065c875b0c2fbdc53ea3cc2c31add126
evengul <44507133+evengul@users.noreply.github.com>
2025-01-29 07:57:26 +0100

    Fix flyway (#48)
    Co-authored-by: evegul <even.gultvedt@sparebank1.no>

--- webapp/src/main/kotlin/app/mcorg/Application.kt
+++ webapp/src/main/kotlin/app/mcorg/Application.kt
@@ -5,13 +5,10 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import app.mcorg.presentation.plugins.*
 import app.mcorg.presentation.configureAppRouter
-import app.mcorg.utils.migrateDb
 import org.slf4j.LoggerFactory
 
 fun main() {
     val logger = LoggerFactory.getLogger(Application::class.java)
-
-    migrateDb(logger)
 
     logger.info("Starting server")
     embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module, watchPaths = getWatchPaths())
