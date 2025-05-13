a5c0487e02215ff430413683dfc7feb844ff1b5f
evegul <even.gultvedt@sparebank1.no>
2025-05-13 14:02:46 +0200

    fix: playwright once again runs single thread

--- webapp/src/test/javascript/playwright.config.ts
+++ webapp/src/test/javascript/playwright.config.ts
@@ -15,12 +15,13 @@ export default defineConfig({
   testDir: './tests',
   timeout: 5 * 60 * 1000,
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: !process.env.CI,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 5 : undefined,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
