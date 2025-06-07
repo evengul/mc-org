d618db42fc32226089e06b3e280c8750c97d56e3
evengul <even.gultvedt@gmail.com>
2025-06-07 12:47:17 +0200

    feat(audit): Update user/profile audit information
    - When user signs in: Update last_login in for the currently signed in user
    - When user edits technical status: Update updated_by and updated_at fields for user
    - When user is created: set the created_by and updated_by fields = current user

---
+++ webapp/src/main/kotlin/app/mcorg/pipeline/profile/UpdatePlayerAuditingInfoStep.kt
@@ -0,0 +1,18 @@
+package app.mcorg.pipeline.profile
+
+import app.mcorg.domain.pipeline.Result
+import app.mcorg.domain.pipeline.Step
+import app.mcorg.pipeline.DatabaseFailure
+import app.mcorg.pipeline.useConnection
+
+
+data class UpdatePlayerAuditingInfoStep(val currentUserName: String) : Step<Int, DatabaseFailure, Unit> {
+    override suspend fun process(input: Int): Result<DatabaseFailure, Unit> {
+        return useConnection {
+            prepareStatement("update users set updated_by = ?, updated_at = now() where id = ?")
+                .apply { setString(1, currentUserName); setInt(2, input) }
+                .executeUpdate()
+            return@useConnection Result.success()
+        }
+    }
+}
