e83b122b849f694afa3bda0fe02dbf3bbaaa634b
evengul <even.gultvedt@gmail.com>
2025-06-07 13:21:18 +0200

    feat(audit): Update task audit information
    - When task is created: set the created_by and updated_by fields = current user
    - When task is created or deleted: Set updated_by = current user in project table
    - When task is updated: set updated_by = current user in task table
    
    completed_at will be set at a later date, when the database is more finalized

--- webapp/src/main/kotlin/app/mcorg/pipeline/task/CreateCountableTaskStep.kt
+++ webapp/src/main/kotlin/app/mcorg/pipeline/task/CreateCountableTaskStep.kt
@@ -10,11 +10,17 @@ sealed interface CreateCountableStepFailure : CreateCountableTaskFailure {
     data class Other(val failure: DatabaseFailure) : CreateCountableStepFailure
 }
 
-data class CreateCountableTaskStep(val projectId: Int) : Step<Pair<String, Int>, CreateCountableStepFailure, Int> {
+data class CreateCountableTaskStep(val projectId: Int, val currentUsername: String) : Step<Pair<String, Int>, CreateCountableStepFailure, Int> {
     override suspend fun process(input: Pair<String, Int>): Result<CreateCountableStepFailure, Int> {
         return useConnection({ CreateCountableStepFailure.Other(it) }) {
-            prepareStatement("insert into task (project_id, name, needed, done, type) values (?, ?, ?, 0, 'COUNTABLE') returning id")
-                .apply { setInt(1, projectId); setString(2, input.first); setInt(3, input.second) }
+            prepareStatement("insert into task (project_id, name, needed, done, type, created_by, updated_by) values (?, ?, ?, 0, 'COUNTABLE', ?, ?) returning id")
+                .apply {
+                    setInt(1, projectId)
+                    setString(2, input.first)
+                    setInt(3, input.second)
+                    setString(4, currentUsername)
+                    setString(5, currentUsername)
+                }
                 .getReturnedId(CreateCountableStepFailure.Other(DatabaseFailure.NoIdReturned))
         }
     }
