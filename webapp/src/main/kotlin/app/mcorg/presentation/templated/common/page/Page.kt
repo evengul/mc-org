55fbc0591be88c23cac22cba550bfead826a1eb1
evengul <even.gultvedt@gmail.com>
2025-10-24 22:28:58 +0200

    fix: Use correct monospace font

--- webapp/src/main/kotlin/app/mcorg/presentation/templated/common/page/Page.kt
+++ webapp/src/main/kotlin/app/mcorg/presentation/templated/common/page/Page.kt
@@ -48,6 +48,19 @@ fun createPage(
                     rel = "stylesheet"
                 }
             }
+            link {
+                rel = "preconnect"
+                href = "https://fonts.googleapis.com"
+            }
+            link {
+                rel = "preconnect"
+                href = "https://fonts.gstatic.com"
+                attributes["crossorigin"] = "true"
+            }
+            link {
+                rel = "stylesheet"
+                href = "https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"
+            }
             meta {
                 content = "width=device-width, initial-scale=1"
                 name = "viewport"
