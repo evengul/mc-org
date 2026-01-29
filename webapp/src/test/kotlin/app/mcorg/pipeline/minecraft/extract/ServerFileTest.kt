392af52eb6185b80e4fa9cb5e3cf6314304233b3
evengul <even.gultvedt@gmail.com>
2026-01-29 10:54:53 +0100

    test: Add a comprehensive test for extracting loot for all supported versions

---
+++ webapp/src/test/kotlin/app/mcorg/pipeline/minecraft/extract/ServerFileTest.kt
@@ -0,0 +1,100 @@
+package app.mcorg.pipeline.minecraft.extract
+
+import app.mcorg.domain.model.minecraft.MinecraftVersion
+import app.mcorg.domain.model.minecraft.MinecraftVersionRange
+import app.mcorg.domain.pipeline.Pipeline
+import app.mcorg.domain.pipeline.Result
+import app.mcorg.domain.pipeline.Step
+import app.mcorg.pipeline.failure.AppFailure
+import app.mcorg.pipeline.minecraft.ExtractRelevantMinecraftFilesStep
+import app.mcorg.pipeline.minecraft.GetAvailableVersionsStep
+import app.mcorg.pipeline.minecraft.GetServerFileStep
+import app.mcorg.pipeline.minecraft.GetServerUrlsStep
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.fail
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.provider.Arguments
+import java.net.URI
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.io.path.createDirectories
+import kotlin.io.path.exists
+import kotlin.io.path.listDirectoryEntries
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+open class ServerFileTest(
+    val whitelistedVersions: MinecraftVersionRange = MinecraftVersionRange.Unbounded
+) {
+    @BeforeAll
+    fun beforeAll() {
+        val basePath = Paths.get("").toAbsolutePath()
+        val resourcesPath = basePath.resolve("src/test/resources/servers/extracted")
+
+        if (resourcesPath.exists() && resourcesPath.listDirectoryEntries().isNotEmpty()) {
+            return
+        }
+
+        runBlocking {
+            Pipeline.create<AppFailure, Unit>()
+                .pipe(GetAvailableVersionsStep)
+                .pipe(GetServerUrlsStep)
+                .pipe(object :
+                    Step<List<Pair<MinecraftVersion.Release, URI>>, AppFailure, List<Pair<MinecraftVersion.Release, Path>>> {
+                    override suspend fun process(input: List<Pair<MinecraftVersion.Release, URI>>): app.mcorg.domain.pipeline.Result<AppFailure, List<Pair<MinecraftVersion.Release, Path>>> {
+                        val innerPipeline = Pipeline.create<AppFailure, Pair<MinecraftVersion.Release, URI>>()
+                            .pipe(GetServerFileStep)
+                            .pipe(ExtractRelevantMinecraftFilesStep {
+                                resourcesPath.resolve(it).createDirectories()
+                            })
+
+                        if (input.isEmpty()) {
+                            println("No server files to process")
+                            return app.mcorg.domain.pipeline.Result.success(emptyList())
+                        }
+
+                        val result = input.map {
+                            val innerResult = innerPipeline.execute(it)
+                            try {
+                                delay(500)
+                            } catch (e: Exception) {
+                                println("Error in processing $e")
+                            }
+                            innerResult
+                        }
+
+                        val errors = result.filterIsInstance<app.mcorg.domain.pipeline.Result.Failure<AppFailure>>()
+                        if (errors.isNotEmpty()) {
+                            return app.mcorg.domain.pipeline.Result.failure(
+                                AppFailure.customValidationError(
+                                    "files",
+                                    "Couldn't process some server files: ${errors.map { it.error }}"
+                                )
+                            )
+                        }
+
+                        val successes =
+                            result.filterIsInstance<Result.Success<Pair<MinecraftVersion.Release, Path>>>()
+                        return Result.success(successes.map { it.value })
+                    }
+                })
+                .execute(Unit)
+        }
+    }
+
+    fun getVersions(): Array<Arguments> {
+        val basePath = Paths.get("").toAbsolutePath()
+        val resourcesPath = basePath.resolve("src/test/resources/servers/extracted")
+        return resourcesPath.listDirectoryEntries()
+            .asSequence()
+            .map { dir -> dir.toString().substringAfterLast("/").substringAfterLast("\\") }
+            .map { MinecraftVersion.Release.fromString(it) }
+            .sortedWith { release, release1 -> release1.compareTo(release) }
+            .filter { whitelistedVersions.contains(it) }
+            .map { Arguments.of(it) }
+            .toList()
+            .toTypedArray()
+            .takeIf { it.isNotEmpty() } ?: run { fail("Directory $resourcesPath not found. Is the BeforeAll not running?") }
+    }
+}
--- webapp/src/test/kotlin/app/mcorg/pipeline/minecraft/extract/loot/ExtractLootTablesTest.kt
+++ webapp/src/test/kotlin/app/mcorg/pipeline/minecraft/extract/loot/ExtractLootTablesTest.kt
@@ -1,21 +1,37 @@
 package app.mcorg.pipeline.minecraft.extract.loot
 
 import app.mcorg.domain.model.minecraft.MinecraftVersion
+import app.mcorg.pipeline.minecraft.extract.ServerFileTest
 import app.mcorg.test.utils.TestUtils
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import java.nio.file.Path
-import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
-class ExtractLootTablesTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ExtractLootTablesTest : ServerFileTest() {
 
-    @Test
-    fun getLootTables() {
+    @ParameterizedTest
+    @MethodSource("getVersions")
+    fun getLootTables(version: MinecraftVersion.Release) {
+
         val loot = TestUtils.executeAndAssertSuccess(
             ExtractLootTables,
-            MinecraftVersion.Release(1, 21, 11) to Path.of("src/test/resources/servers/extracted")
+            version to Path.of("src/test/resources/servers/extracted/${version.toString().replace(".0", "")}")
         )
 
-        assertEquals(1277, loot.size)
-    }
+        assertNotEquals(0, loot.size)
 
+        loot.forEach { source ->
+            source.requiredItems.forEach {
+                assertNotEquals(it.id, it.name, "Item id and name are equal for id: ${it.id} in loot table: ${source.filename}")
+                assertNotEquals("", it.name, "Item name is empty for id: ${it.id} in loot table: ${source.filename}")
+            }
+            source.producedItems.forEach {
+                assertNotEquals(it.id, it.name, "Item id and name are equal for id: ${it.id} in loot table: ${source.filename}")
+                assertNotEquals("", it.name, "Item name is empty for id: ${it.id} in loot table: ${source.filename}")
+            }
+        }
+    }
 }
